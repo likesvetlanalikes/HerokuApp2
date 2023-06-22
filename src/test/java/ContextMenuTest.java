6055237191f676de6d97038ba7b3f3fcc6740247
svetlana <svetlana_040285@mail.ru>
2023-06-22 20:32:47 +0300

    Context Menu (added alertTextValidationTesT)

--- src/test/java/ContextMenuTest.java
+++ src/test/java/ContextMenuTest.java
@@ -1,15 +1,29 @@
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
 
 public class ContextMenuTest extends BaseTest {
 
         @Test
-            public void checkRightButtonClick() {
-        driver.get("https://the-internet.herokuapp.com/context_menu");
-                Actions actions = new Actions(driver);
-                actions.contextClick(driver.findElement(By.id("hot-spot"))).build().perform();
 
+        public void checkRightButtonClickTest() {
+        driver.get("https://the-internet.herokuapp.com/context_menu");
+        Actions actions = new Actions(driver);
+        actions.contextClick(driver.findElement(By.id("hot-spot"))).build().perform();
+
+    }
+
+    @Test
+    public void alertTextValidationTesT() {
+
+        driver.get("https://the-internet.herokuapp.com/context_menu");
+        Actions actions = new Actions(driver);
+        actions.contextClick(driver.findElement(By.id("hot-spot"))).build().perform();
+        Alert alert = driver.switchTo().alert();
+        assertEquals(alert.getText(),"You selected a context menu", "Текст алерта не корректен");
+        alert.accept();
     }
 }
