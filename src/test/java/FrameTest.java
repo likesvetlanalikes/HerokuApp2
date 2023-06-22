ec26d255dbb7ff9eb00beb39a17d503acf2add66
svetlana <svetlana_040285@mail.ru>
2023-06-23 01:08:03 +0300

    (added FrameTest)

--- src/test/java/FrameTest.java
+++ src/test/java/FrameTest.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 public class FrameTest extends BaseTest {
     @Test
-    public void  IframeContentTest() {
+    public void  iframeContentTest() {
         driver.get("http://the-internet.herokuapp.com/iframe");
         WebDriver frame = driver.switchTo().frame(driver.findElement(By.id("mce_0_ifr")));
         frame.findElement(By.xpath("//body[@id = 'tinymce']/p")).getText();
